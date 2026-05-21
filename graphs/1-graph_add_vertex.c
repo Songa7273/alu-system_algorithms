1921f2a5b84694cc14bc4e5820bdaa95705e5342
Songa7273 <songa7273@example.com>
2026-05-21 06:46:19 -0700

    Fix vertex struct usage (remove nb_edges)

--- graphs/1-graph_add_vertex.c
+++ graphs/1-graph_add_vertex.c
@@ -1,77 +1,32 @@
+#include "graphs.h"
 #include <stdlib.h>
 #include <string.h>
-#include "graphs.h"
 
-/**
- * add_vertex_end - adds a vertex at the end of the list
- * @head: pointer to head of vertex list
- * @new: new vertex to add
- *
- * Return: pointer to added vertex
- */
-static vertex_t *add_vertex_end(vertex_t **head, vertex_t *new)
-{
-	vertex_t *tmp;
-	vertex_t *last;
-
-	tmp = *head;
-	last = NULL;
-
-	while (tmp)
-	{
-		last = tmp;
-		tmp = tmp->next;
-	}
-
-	if (last == NULL)
-		*head = new;
-	else
-		last->next = new;
-
-	return (new);
-}
-
-/**
- * graph_add_vertex - adds a vertex to a graph
- * @graph: pointer to graph
- * @str: string to store in vertex
- *
- * Return: pointer to new vertex, or NULL on failure
- */
 vertex_t *graph_add_vertex(graph_t *graph, const char *str)
 {
-	vertex_t *new, *tmp;
+    vertex_t *vertex;
 
-	if (graph == NULL || str == NULL)
-		return (NULL);
+    if (!graph || !str)
+        return NULL;
 
-	tmp = graph->vertices;
-	while (tmp)
-	{
-		if (strcmp(tmp->content, str) == 0)
-			return (NULL);
-		tmp = tmp->next;
-	}
+    vertex = malloc(sizeof(vertex_t));
+    if (!vertex)
+        return NULL;
 
-	new = malloc(sizeof(vertex_t));
-	if (new == NULL)
-		return (NULL);
+    vertex->content = strdup(str);
+    if (!vertex->content)
+    {
+        free(vertex);
+        return NULL;
+    }
 
-	new->content = strdup(str);
-	if (new->content == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
+    vertex->index = graph->nb_vertices;
+    vertex->edges = NULL;
 
-	new->edges = NULL;
-	new->next = NULL;
+    vertex->next = graph->vertices;
+    graph->vertices = vertex;
 
-	new->index = graph->nb_vertices;
+    graph->nb_vertices++;
 
-	add_vertex_end(&graph->vertices, new);
-
-	graph->nb_vertices++;
-
-	return (new);
+    return vertex;
 }
