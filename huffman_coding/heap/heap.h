7055ee130782e3b9e71415ad75da264b54117860
Songa7273 <songa7273@example.com>
2026-06-04 07:35:53 -0700

    extracts

--- huffman_coding/heap/heap.h
+++ huffman_coding/heap/heap.h
@@ -35,5 +35,6 @@ heap_t *heap_create(int (*data_cmp)(void *, void *));
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent,
 	void *data);
 binary_tree_node_t *heap_insert(heap_t *heap, void *data);
+void *heap_extract(heap_t *heap);
 
 #endif /* HEAP_H */
--- huffman_coding/main.c
+++ huffman_coding/main.c
@@ -3,27 +3,26 @@
 
 int int_cmp(void *a, void *b)
 {
-	int x = *(int *)a;
-	int y = *(int *)b;
-	return (x - y);
+	return (*(int *)a - *(int *)b);
 }
 
 int main(void)
 {
 	heap_t *heap;
-	int values[] = {34, 2, 45, 23, 76};
+	int arr[] = {34, 2, 45, 23, 76};
 	size_t i;
+	int *extracted;
 
 	heap = heap_create(int_cmp);
 	if (!heap)
 		return (1);
 
 	for (i = 0; i < 5; i++)
-	{
-		heap_insert(heap, &values[i]);
-		printf("Inserted: %d\n", values[i]);
-	}
+		heap_insert(heap, &arr[i]);
 
+	extracted = heap_extract(heap);
+	printf("Extracted: %d\n", *extracted);
 	printf("Heap size: %lu\n", heap->size);
+
 	return (0);
 }
