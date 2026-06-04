fcfcbe2fbc17bf6ff39bc7be639acf7f390b0f35
Songa7273 <songa7273@example.com>
2026-06-04 08:28:27 -0700

    priority queue

--- huffman_coding/huffman_priority_queue.c
+++ huffman_coding/huffman_priority_queue.c
@@ -3,51 +3,26 @@
 #include <stdlib.h>
 
 /**
- * create_nested_node - creates a nested binary tree node
- * @symbol: pointer to symbol
- *
- * Return: pointer to binary_tree_node_t or NULL
+ * symbol_cmp - compares two symbols inside heap nodes
  */
-static binary_tree_node_t *create_nested_node(symbol_t *symbol)
+static int symbol_cmp(void *a, void *b)
 {
-	return (binary_tree_node(NULL, symbol));
-}
-
-/**
- * cmp_nested - compares two nested heap nodes
- * @a: first node
- * @b: second node
- *
- * Return: difference based on frequency
- */
-static int cmp_nested(void *a, void *b)
-{
-	binary_tree_node_t *na = (binary_tree_node_t *)a;
-	binary_tree_node_t *nb = (binary_tree_node_t *)b;
-
-	symbol_t *sa = (symbol_t *)na->data;
-	symbol_t *sb = (symbol_t *)nb->data;
+	symbol_t *sa = (symbol_t *)a;
+	symbol_t *sb = (symbol_t *)b;
 
 	return ((int)(sa->freq - sb->freq));
 }
 
 /**
- * huffman_priority_queue - builds a min heap (priority queue)
- * @data: array of characters
- * @freq: array of frequencies
- * @size: number of elements
- *
- * Return: pointer to heap or NULL on failure
+ * huffman_priority_queue - builds priority queue
  */
 heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size)
 {
 	heap_t *heap;
-	binary_tree_node_t *nested;
-	binary_tree_node_t *heap_node;
 	symbol_t *symbol;
 	size_t i;
 
-	heap = heap_create(cmp_nested);
+	heap = heap_create(symbol_cmp);
 	if (!heap)
 		return (NULL);
 
@@ -57,15 +32,8 @@ heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size)
 		if (!symbol)
 			return (NULL);
 
-		nested = create_nested_node(symbol);
-		if (!nested)
-			return (NULL);
-
-		heap_node = binary_tree_node(NULL, nested);
-		if (!heap_node)
-			return (NULL);
-
-		heap_insert(heap, heap_node);
+		/* IMPORTANT: store symbol_t directly in heap node */
+		heap_insert(heap, symbol);
 	}
 
 	return (heap);
