c65bb8cdbf886cfc9cd1e5eaf4848ddb33ed61bc
Songa7273 <songa7273@example.com>
2026-06-04 09:24:20 -0700

    priority

--- huffman_coding/huffman_priority_queue.c
+++ huffman_coding/huffman_priority_queue.c
@@ -3,12 +3,15 @@
 #include <stdlib.h>
 
 /**
- * symbol_cmp - compares two symbols inside heap nodes
+ * symbol_cmp - compares two heap nodes containing symbols
  */
 static int symbol_cmp(void *a, void *b)
 {
-	symbol_t *sa = (symbol_t *)a;
-	symbol_t *sb = (symbol_t *)b;
+	binary_tree_node_t *na = (binary_tree_node_t *)a;
+	binary_tree_node_t *nb = (binary_tree_node_t *)b;
+
+	symbol_t *sa = (symbol_t *)na->data;
+	symbol_t *sb = (symbol_t *)nb->data;
 
 	return ((int)(sa->freq - sb->freq));
 }
@@ -19,6 +22,7 @@ static int symbol_cmp(void *a, void *b)
 heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size)
 {
 	heap_t *heap;
+	binary_tree_node_t *node;
 	symbol_t *symbol;
 	size_t i;
 
@@ -32,8 +36,11 @@ heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size)
 		if (!symbol)
 			return (NULL);
 
-		/* IMPORTANT: store symbol_t directly in heap node */
-		heap_insert(heap, symbol);
+		node = binary_tree_node(NULL, symbol);
+		if (!node)
+			return (NULL);
+
+		heap_insert(heap, node);
 	}
 
 	return (heap);
